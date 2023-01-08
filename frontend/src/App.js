8e96dfb7edfd45c6057324e3a1d73a8489342d7a
guptavarun619 <guptavarun619@gmail.com>
2023-01-08 16:13:03 +0530

    🚀 Initial frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-5xl font-bold text-center">Money Tracker</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
